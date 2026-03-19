5c52671483a5d6f49cfd03653ae8860d07e7110e
MurilloHMS <murillohenriquemelo@outlook.com>
2026-03-19 17:22:54 -0300

    feat: incluído declaracoes do disk

---
+++ include/modules/disk.h
@@ -0,0 +1,23 @@
+#ifndef WINDIXER_DISK_H
+#define WINDIXER_DISK_H
+
+#define MAX_VOLUMES 26
+
+typedef struct {
+    char letter;
+    char label[64];
+    char fs_type[16];
+    char drive_type[32];
+    ULONGLONG total;
+    ULONGLONG free;
+    int accessible;
+} VolumeInfo;
+
+typedef struct {
+    VolumeInfo volumes[MAX_VOLUMES];
+    int count;
+} VolumeList;
+
+void menu_disk(void);
+
+#endif
