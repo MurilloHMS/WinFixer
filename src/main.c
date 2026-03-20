9c742c86494e6e962d77fa68d06f0fb2d6119bcf
MurilloHMS <murillohenriquemelo@outlook.com>
2026-03-20 12:08:12 -0300

    feat: incluído sysinfo no menu

--- src/main.c
+++ src/main.c
@@ -3,21 +3,24 @@
 #include "ui.h"
 #include "modules/apps.h"
 #include "modules/disk.h"
+#include "modules/sysinfo.h"
 
 int main(void) {
     const char *items[] = {
         "Gerenciador de Aplicativos",
         "Analise de Disco",
+        "Informacoes do Sistema",
         "Sair"
     };
 
     while (1) {
-        int choice = ui_run_menu("Menu Principal", items, 3, NULL);
-        if (choice == -1 || choice == 2) break;
+        int choice = ui_run_menu("Menu Principal", items, 4, NULL);
+        if (choice == -1 || choice == 3) break;
 
         switch (choice) {
             case 0: menu_apps(); break;
             case 1: menu_disk(); break;
+            case 2: menu_sysinfo(); break;
         }
     }
 
